17af974ebc6c371e15afe5688df1e025b17c31de
Igor Noek <megapihar>
2020-04-19 20:40:54 +0500

    Initial Commit

---
+++ MySound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySound
+//
+//  Created by Игорь Ноек on 19.04.2020.
+//  Copyright © 2020 Игорь Ноек. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
