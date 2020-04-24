4cb568d5383511eb43643b03d2a24bd5c520a595
Igor Noek <megapihar>
2020-04-24 23:38:32 +0500

    added outlets

--- MySound/Search/Track Detail View/TrackDetailView.swift
+++ MySound/Search/Track Detail View/TrackDetailView.swift
@@ -11,8 +11,52 @@ import UIKit
 
 class TrackDetailView: UIView {
     
+    @IBOutlet weak var trackImageView: UIImageView!
+    
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationTimeLabel: UILabel!
+    
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        trackImageView.backgroundColor = .red
+
     }
+    
+
+    
+    @IBAction func dragDownButtonTapped(_ sender: UIButton) {
+        
+        self.removeFromSuperview()
+        
+    }
+    
+    
+    @IBAction func handleCurrentTimeSlider(_ sender: UISlider) {
+    }
+    
+    
+    @IBAction func handleVolumeSlider(_ sender: UISlider) {
+    }
+    
+    
+    @IBAction func previousTrackButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func playPauseButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func nextTrackButtonTapped(_ sender: UIButton) {
+    }
+    
+    
     
 }
